e5516508721187ecfc421fb7174e28aa1f980c32
Dallin Cawley <dallin.cawley@gmail.com>
2020-04-04 19:10:47 -0600

    Added a for loop

--- League_Of_Legends_API/views.py
+++ League_Of_Legends_API/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+player = [
+    {
+        'summoner_name': 'TheFlyingBannana',
+        'summoner_level': 142,
+        'summoner_icon_id': 192,
+        'games_won': 1000
+    },
+    {
+        'summoner_name': 'Taegondy',
+        'summoner_level': 200,
+        'summoner_icon_id': 36,
+        'games_won': 2000
+    }
+]
 
-# Create your views here.
+
 def home(request):
-    return HttpResponse('<h1>League Home</h1>')
+    summoners = {
+        'player': player
+    }
+    return render(request, 'League_Of_Legends_API/home.html', summoners)
 
 
 def results(request):
