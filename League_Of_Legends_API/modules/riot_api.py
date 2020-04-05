6e6f72d9c594977d833adb102db681287f1543ee
Dallin-Cawley <dallincawley@gmail.com>
2020-04-05 14:33:50 -0600

    Added api call functionality

---
+++ League_Of_Legends_API/modules/riot_api.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+
+def riot_API_summoner_info(name):
+    url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name
+    args = {'api_key': 'RGAPI-72e44e7e-5527-4b1a-89f6-7d011b02dff6'}
+    riot_api_summoner_GET = requests.get(url, params=args)
+
+    return from_json(riot_api_summoner_GET.content)
+
+
+def from_json(json_string):
+    return json.loads(json_string)
--- League_Of_Legends_API/views.py
+++ League_Of_Legends_API/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-player = [
-    {
-        'summoner_name': 'TheFlyingBannana',
-        'summoner_level': 142,
-        'summoner_icon_id': 192,
-        'games_won': 1000
-    },
-    {
-        'summoner_name': 'Taegondy',
-        'summoner_level': 200,
-        'summoner_icon_id': 36,
-        'games_won': 2000
-    }
-]
+from League_Of_Legends_API.modules.riot_api import riot_API_summoner_info
 
 
 def home(request):
     summoners = {
-        'player': player
+        'url': riot_API_summoner_info('TheFlyingBannana')
     }
     return render(request, 'League_Of_Legends_API/home.html', summoners)
 
