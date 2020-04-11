ff6971c6d3210d795af8b0ec7310b8874b718c0e
Dallin Cawley <dallin.cawley@gmail.com>
2020-04-11 00:34:58 -0600

    removed unecessary code

--- League_Of_Legends_API/urls.py
+++ League_Of_Legends_API/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='league-home'),
     path('results/', views.results, name='results'),
-    path('results/<summoner_name>', views.results, name="get-summoner-info")
+
 ]
